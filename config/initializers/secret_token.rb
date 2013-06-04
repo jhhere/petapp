45ba6ce9c5c3868918f96d5757a7ba7adfebf171
joyce <joyicecloud@gmail.com>
2013-06-04 14:58:06 -0700

    Bundle installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petapp::Application.config.secret_token = '9cca95f57c6103adab83df87c64bd61c57031dbf406ad9802422925b591769b4f4088cc0e1de764e24959b1ce4918825816e6986101207c29f503bb147ff6799'
