8cf5cf6956e632a448e9b76430cdf0a49e24ddea
joyce <joyicecloud@gmail.com>
2013-06-05 18:05:33 -0700

    various client controller updates

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,20 +1,44 @@
 class ClientsController < ApplicationController
   def index
+  	@clients = Client.all
   end
 
   def show
+    @client = Client.find(params[:id])
+    
   end
 
   def new
+    @client = Client.new
+
   end
 
   def create
+    @client = Client.new(params[:client])
+
+    if @client.save
+      redirect_to @client
+    else
+      render 'new'
+    end
+
   end
 
   def edit
+    @client = Client.find(params[:id])
+
   end
 
   def update
+    @client = Client.find(params[:id])
+
+    @client.update_attributes(params[:client])
+
+    if @client.save
+      redirect_to @client
+    else
+      render 'edit'
+    end
   end
 
   def delete
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,6 @@
 class Client < ActiveRecord::Base
   # attr_accessible :title, :body
+   attr_accessible :first_name, :last_name, :home_phone, :work_phone, :rate, :notes
 
   has_many :pets
   has_many :addresses
